0eee5b2c18c2efc657ce7c88a34834e5ea2ddac1
meldyssey <meldyssey15@gmail.com>
2024-11-28 17:45:40 +0900

    현재까지 작업 커밋

--- frontend/src/components/product/colognesHome/ColognesNav.js
+++ frontend/src/components/product/colognesHome/ColognesNav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "../../../scss/product/colongnesNav.scss";
 
 const ColognesNav = () => {
