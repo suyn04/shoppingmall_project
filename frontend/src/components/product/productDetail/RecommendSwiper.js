2eb4695671ae59461ea1fe730d0405f035250117
meldyssey <meldyssey15@gmail.com>
2024-11-28 15:23:34 +0900

    지금까지 작업한 것 commit

--- frontend/src/components/product/productDetail/RecommendSwiper.js
+++ frontend/src/components/product/productDetail/RecommendSwiper.js
@@ -6,7 +6,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import "../../../scss/swiperStyles.css";
+import "../../../scss/product/swiperStyles.css";
 
 // import required modules
 import { Pagination, Navigation } from "swiper/modules";
