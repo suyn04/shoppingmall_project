56b7a6c4bda7bcaff0e7e68263e573ffe5bb6ca2
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-11-29 16:06:09 +0900

    11/29 세라님 라우트 수정

--- frontend/src/Mypage.js
+++ frontend/src/Mypage.js
@@ -1,35 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
-import Navbar from './components/mypage/Navbar';
+import { Route, Routes } from 'react-router-dom';
 import AddressList from './components/mypage/AddressList';
+import MyInfoTemp from './components/mypage/MyInfoTemp';
 import MyInfo from './components/mypage/MyInfo';
 import MyinfoEdit from './components/mypage/MyinfoEdit';
 import ViewOrders from './components/mypage/ViewOrders';
 import SignIn from './components/mypage/SignIn';
 import SignUp from './components/mypage/SignUp';
 
-function App() {
+function MyPage() {
     return (
         <>
-            <Navbar />
-            <div className="App">
-                <Outlet />
-                <div className="content">
-                    <Routes>
-                        <Route path="/signIn" element={<SignIn />} />
-                        <Route path="/signUp" element={<SignUp />} />
-                    </Routes>
-                    <Routes>
-                        {/* 기본라우트 */}
-                        <Route path="/myInfo" element={<MyInfo />} />
-                        <Route path="/addressList" element={<AddressList />} />
-                        <Route path="/myinfoEdit" element={<MyinfoEdit />} />
-                        <Route path="/viewOrders" element={<ViewOrders />} />
-                    </Routes>
-                </div>
-            </div>
+            <Routes>
+                <Route path="/signIn" element={<SignIn />} />
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/myPage" element={<MyInfoTemp />}>
+                    <Route path="" element={<MyInfo />} />
+                    <Route path="addressList" element={<AddressList />} />
+                    <Route path="myinfoEdit" element={<MyinfoEdit />} />
+                    <Route path="viewOrders" element={<ViewOrders />} />
+                </Route>
+            </Routes>
         </>
     );
 }
 
-export default App;
+export default MyPage;
