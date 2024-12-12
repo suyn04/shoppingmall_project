5afa444d2ce59b19b6d97f246604a13b756ce669
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-12-12 15:36:15 +0900

    12/12 매출분석 완~~ 신난다~~

--- backend/controller/analysis.js
+++ backend/controller/analysis.js
@@ -1,23 +1,81 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../db");
-const fs = require("fs");
-const path = require("path");
 
 module.exports = () => {
-    router.get("/", async (req, res) => {
-        console.log(`/ 진입 확인`); //정상작동 확인
+  router.get("/day", async (req, res) => {
+    console.log(`매출 /day 진입 확인`)
 
-        try {
-            const [ret] = await conn.execute(
-                "SELECT * from orders WHERE order_status = '배송완료'"
-            );
-            res.json(ret);
-        } catch (err) {
-            console.error("sql 실패 : ", err.message);
-            res.status(500).send("db 오류");
-        }
-    });
+    try {
+      const [ret] = await conn.execute(
+        `SELECT 
+          DATE(order_date) AS order_date, 
+          SUM(CAST(order_total AS UNSIGNED)) AS total_amount
+        FROM orders
+        WHERE 
+          order_status = '배송완료'
+          AND order_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
+        GROUP BY 
+          DATE(order_date)
+        ORDER BY 
+          order_date`
+      )
+      // console.log(ret)
+      res.json(ret)
+    }catch(err){
+      console.error("SQL 실패: ", err.message)
+      res.status(500).send("DB 오류")
+    }
+  })
 
-    return router;
-};
+  router.get("/month", async (req, res) => {
+    console.log(`매출 /month 진입 확인`)
+
+    try{
+      const [ret] = await conn.execute(
+        `SELECT 
+          *, DATE_FORMAT(order_date, '%Y-%m') AS order_month,
+          SUM(CAST(order_total AS UNSIGNED)) AS total_amount
+        FROM orders
+        WHERE 
+          order_status = '배송완료'
+          AND YEAR(order_date) = YEAR(CURDATE())
+          AND MONTH(order_date) < MONTH(CURDATE())
+        GROUP BY 
+          DATE_FORMAT(order_date, '%Y-%m')
+        ORDER BY 
+          order_month`
+      )
+      res.json(ret)
+    }catch(err){
+      console.error("SQL 실패: ", err.message)
+      res.status(500).send("DB 오류")
+    }
+  })
+
+  router.get("/year", async (req, res) => {
+    console.log(`매출 /year 진입 확인`)
+
+    try{
+      const [ret] = await conn.execute(
+        `SELECT 
+            *, YEAR(order_date) AS order_year,
+            SUM(CAST(order_total AS UNSIGNED)) AS total_amount
+          FROM orders
+          WHERE 
+            order_status = '배송완료'
+            AND YEAR(order_date) >= YEAR(CURDATE()) - 3
+          GROUP BY 
+            YEAR(order_date)
+          ORDER BY 
+            order_year`
+      )
+      res.json(ret)
+    }catch(err){
+      console.error("SQL 실패: ", err.message)
+      res.status(500).send("DB 오류")
+    }
+  })
+
+  return router
+}
