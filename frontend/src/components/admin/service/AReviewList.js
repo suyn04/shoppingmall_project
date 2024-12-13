a7c0cd0168e27983169f9870d040962529f7cdf8
suyn04 <63181309+suyn04@users.noreply.github.com>
2024-12-13 11:22:14 +0900

    no message

--- frontend/src/components/admin/service/AReviewList.js
+++ frontend/src/components/admin/service/AReviewList.js
@@ -28,7 +28,7 @@ const AReviewList = () => {
         console.log('전달된 ID:', id); // 클릭한 리뷰의 ID 확인
         navigate(`/admin/areviewdetail/${id}`); // 상세보기 페이지로 이동
     };
-    // if (loading) return <p>로딩 중...</p>;
+    if (loading) return <p>로딩 중...</p>;
     if (error) return <p>{error}</p>;
 
     return (
